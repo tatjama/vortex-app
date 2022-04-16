16dfbf566f08881722b724a59de8f1b08890e128
Tanja <tanja120a@gmail.com>
2022-04-16 21:28:38 +0200

    Create navigation span

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -8,27 +8,40 @@ const Navigation = () => {
             <ul className={classes.navigation}>
                 <li>
                     <NavLink exact={true}  style={(isActive) => ({
-                backgroundColor: isActive ? "rgb(7, 8, 49)" : "rgb(187, 188, 243)",
+                backgroundColor: isActive ? "rgb(54, 54, 85)" : "rgb(222, 222, 238)",
                 color: isActive ? "rgb(187, 188, 243)" : "rgb(123, 124, 180)"
-              })}  to={`/`}>1</NavLink>
+              })}  to={`/`}>1
+              <span className={classes.span}>Genre</span>
+              </NavLink>
+                
                 </li>
                 <li>
                     <NavLink  style={(isActive) => ({
-                backgroundColor: isActive ? "rgb(7, 8, 49)" : "rgb(187, 188, 243)",
+                backgroundColor: isActive ? "rgb(54, 54, 85)" : "rgb(222, 222, 238)",
                 color: isActive ? "rgb(187, 188, 243)" : "rgb(123, 124, 180)"
-              })}  to={`/subgenre`}>2</NavLink>
+              })}  to={`/subgenre`}>2
+              <span className={classes.span}>Subgenre</span>
+              </NavLink>
+                
                 </li>
                 <li>
                     <NavLink  style={(isActive) => ({
-                backgroundColor: isActive ? "rgb(7, 8, 49)" : "rgb(187, 188, 243)",
+                backgroundColor: isActive ? "rgb(54, 54, 85)" : "rgb(222, 222, 238)",
                 color: isActive ? "rgb(187, 188, 243)" : "rgb(123, 124, 180)"
-              })}  to={`/addSubgenre`}>3</NavLink>
+              })}  to={`/addSubgenre`}>3
+              <span className={classes.span}>Add new Subgenre</span>
+              </NavLink>               
+                
                 </li>
                 <li>
                     <NavLink  style={(isActive) => ({
-                backgroundColor: isActive ? "rgb(7, 8, 49)" : "rgb(187, 188, 243)",
+                backgroundColor: isActive ? "rgb(54, 54, 85)" : "rgb(222, 222, 238)",
                 color: isActive ? "rgb(187, 188, 243)" : "rgb(123, 124, 180)"
-              })}  to={`/information`}>4</NavLink>
+              })}  to={`/information`}>4
+              <span className={classes.span}>Information</span>
+              </NavLink>
+                
+                
                 </li>
             </ul>
             
