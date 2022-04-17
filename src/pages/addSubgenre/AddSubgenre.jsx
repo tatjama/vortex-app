2ea83e8c5e3eb571418934edac757dbf5662e42d
Tanja <tanja120a@gmail.com>
2022-04-17 18:22:50 +0200

    Create AddNewSubgenre

--- src/pages/addSubgenre/AddSubgenre.jsx
+++ src/pages/addSubgenre/AddSubgenre.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react';
+import classes from './AddSubgenre.module.css';
 const AddSubgenre = () => {
+
+    const [ subgenreName, setSubgenreName ] = useState('');
+    const [ isDescription, setIsDescription ] = useState(false);
+
+    /*const handleChange = (event) => {
+        console.log(event.target.value);
+        setSubgenreName(event.target.value);
+    }*/
+ 
     return (
-        <div>
-            <h1>AddSubgenre</h1>
-        </div>
+        <form className={classes.form}>
+            <input 
+                type = "text" 
+                placeholder = "Subgenre name" 
+                name = "subgenreName" 
+                value = {subgenreName} 
+                onChange={(e) => {setSubgenreName(e.target.value)}}
+            />
+            
+            <label className={classes.checkbox_container}>
+                Description is required for this subgenre
+                <input type = "checkbox" checked = {isDescription} onChange = {() => setIsDescription(!isDescription)}/>
+                <span className={classes.checkmark}></span>
+            </label>
+        </form>
     )
 }
 
--- src/pages/subgenre/Subgenre.jsx
+++ src/pages/subgenre/Subgenre.jsx
@@ -1,8 +1,42 @@
+import { useState } from 'react';
+//import classes from './Genre.module.css'; 
+
+import Button from '../../components/button/Button';
+
 const Subgenre = () => {
-    return (
-        <div>
-            <h1>Subgenre</h1>
-        </div>
+
+    const [styleDescription, setStyleDescription] = useState('btn');
+    const [activeButton, setActiveButton] = useState(null);
+    const handleChooseGenre = (id) => {
+        console.log('clicked' + id)
+        //setStyleDescription('btn_active');
+        setActiveButton(id);
+    }
+
+    const arrayOfButtons = ['Subgenre 1', 'Subgenre 2', 'Subgenre 3', 'Subgenre 4', 'Subgenre 5', 'Subgenre 6', 
+    'Subgenre 7', 'Subgenre 8',]
+
+    return(
+        <>
+            {
+                arrayOfButtons.map((button, index) => {
+                    return <Button 
+                                styleDescription = { (activeButton === index)? 'btn_active': "btn"} 
+                                handleChooseGenre = {() => handleChooseGenre(index)} 
+                                text = {button}
+                                id = {index}
+                                key = {index}
+                            />
+                })
+            }
+                            <Button 
+                                styleDescription = { (activeButton === "addNew")? 'btn_active': "btn"} 
+                                handleChooseGenre = {() => handleChooseGenre("addNew")} 
+                                text = "Add new"
+                                id = "addNew"
+                                key = "addNew"
+                            />
+        </>
     )
 }
 
